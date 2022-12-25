c43439fd006e3c2556d7921eb35bf7938fdaa922
Angelo Chaknis <angelopalmtree@gmail.com>
2022-12-25 15:31:31 -0700

    feat: add gamePlay func logic

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const gameChoices = ["Rock", "Paper", "Scissors"];
+const gameChoices = ["rock", "paper", "scissors"];
 
 const getComputerChoice = () => {
   const random = Math.floor(Math.random() * gameChoices.length);
@@ -11,3 +11,25 @@ const getPlayerChoice = () => {
   ).toLowerCase();
   return playerChoice;
 };
+
+const gamePlay = () => {
+  let computerPick = getComputerChoice();
+  let playerPick = getPlayerChoice();
+  if (playerPick === computerPick) {
+    console.log("Tie");
+  } else if (
+    (computerPick == "rock" && playerPick == "scissors") ||
+    (computerPick == "scissors" && playerPick == "paper") ||
+    (computerPick == "paper" && playerPick == "rock")
+  ) {
+    console.log(
+      `Computer wins! With its ${computerPick} against your ${playerPick}`
+    );
+  } else if (
+    console.log(
+      `You win! With your ${playerPick} against the computer's ${computerPick}`
+    )
+  );
+};
+
+gamePlay();
